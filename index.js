f148823d15ca8a279e323c3317ad7bef29eea4ab
Jacques Sovoessi <jacques.sovoessi@gmail.com>
2025-05-20 11:53:49 +0200

    added public folder

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(express.static("public"));
+// Middleware to parse JSON and URL-encoded data
+// This is important for handling form submissions and JSON requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
